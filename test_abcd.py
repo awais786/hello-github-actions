c39473afb86b5767bff75e655779e2ee7b0616f2
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-06-08 15:31:44 +0500

    a

--- test_abcd.py
+++ test_abcd.py
@@ -60,3 +60,8 @@ def test_wallet_spend_cash5(wallet):
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     with pytest.raises(InsufficientAmount):
         empty_wallet.spend_cash(100)
+
+
+
+def test_default_initial_amount123(empty_wallet):
+    assert empty_wallet.balance == 111
